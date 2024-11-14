44e20ffe61acedfb083ec1cfba920ca0513fa677
yuuka-kinto-ktc <cw-yuka.kumai@kinto-technologies.com>
2024-11-14 18:00:10 +0900

    config変更 URL振り分け

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'test_vite',
+  base: import.meta.env.DEV
+    ? '/'
+    : 'test_vite',
   plugins: [react()],
 })
